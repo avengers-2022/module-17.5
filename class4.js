3de44406fd8403b5b68b2276d35f1a91d63fed5c
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-09 11:24:52 +0600

    class 4 done

---
+++ class4.js
@@ -0,0 +1,14 @@
+for (var roastGiven = 0; roastGiven <= 5; roastGiven++) {
+    console.log(roastGiven, "Roast Den!!!");
+}
+
+
+
+
+for (var number = 0; number <= 10; number++) {
+    if (number % 2 == 0) {
+        console.log(number, "This number is Even!!!");
+    } else {
+        console.log(number, "This numbe is Odd!!!");
+    }
+}
