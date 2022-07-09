8420d1a5ccf0878cf79f07a96efefcf8d3681da1
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-09 11:06:24 +0600

    class 3 done

---
+++ class3.js
@@ -0,0 +1,11 @@
+var number = 0;
+
+while (number < 4) {
+    if (number % 2 == 0) {
+        console.log(number, "is Even Number!!!");
+    } else {
+        console.log(number, "is Odd Number!!!");
+    }
+    number++
+
+}
